762c2ccda5543108f4b9950be31c72870ce3f4ed
senseiwells <wellsm488@gmail.com>
2025-07-20 22:03:04 +0100

    fix: Compatability with polymer

--- arcade-extensions/src/main/kotlin/net/casual/arcade/extensions/EntityExtension.kt
+++ arcade-extensions/src/main/kotlin/net/casual/arcade/extensions/EntityExtension.kt
@@ -6,6 +6,7 @@ package net.casual.arcade.extensions
 
 import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.entity.Entity
+import org.jetbrains.annotations.ApiStatus.Internal
 
 public abstract class EntityExtension private constructor(
     private val provider: () -> Entity
@@ -16,6 +17,11 @@ public abstract class EntityExtension private constructor(
     public constructor(entity: Entity): this(entityToProvider(entity))
 
     private companion object {
+        @Internal
+        @JvmField
+        @Suppress("unused")
+        val SHOULD_ATTACH_EXTENSION: ThreadLocal<Boolean> = ThreadLocal.withInitial { true }
+
         fun entityToProvider(entity: Entity): () -> Entity {
             if (entity is ServerPlayer) {
                 val connection = entity.connection
