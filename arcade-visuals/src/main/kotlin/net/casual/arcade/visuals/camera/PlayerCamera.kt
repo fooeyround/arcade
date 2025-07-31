5e79673a8a8da90eafc4600133f1b3ec7a32638e
senseiwells <wellsm488@gmail.com>
2025-07-31 22:52:18 +0100

    feat: Fix PlayerCamera letting you punch yourself

--- arcade-visuals/src/main/kotlin/net/casual/arcade/visuals/camera/PlayerCamera.kt
+++ arcade-visuals/src/main/kotlin/net/casual/arcade/visuals/camera/PlayerCamera.kt
@@ -8,8 +8,7 @@ import eu.pb4.polymer.virtualentity.api.ElementHolder
 import eu.pb4.polymer.virtualentity.api.VirtualEntityUtils
 import eu.pb4.polymer.virtualentity.api.attachment.ManualAttachment
 import eu.pb4.polymer.virtualentity.api.elements.SimpleEntityElement
-import net.casual.arcade.scheduler.GlobalTickedScheduler
-import net.casual.arcade.utils.TimeUtils.Ticks
+import net.casual.arcade.utils.ClientboundPlayerInfoUpdatePacket
 import net.casual.arcade.utils.math.location.Location
 import net.casual.arcade.utils.math.location.LocationWithLevel
 import net.casual.arcade.utils.math.location.LocationWithLevel.Companion.asLocation
@@ -20,6 +19,8 @@ import net.casual.arcade.visuals.extensions.PlayerCameraExtension.Companion.came
 import net.minecraft.network.protocol.Packet
 import net.minecraft.network.protocol.game.ClientGamePacketListener
 import net.minecraft.network.protocol.game.ClientboundGameEventPacket
+import net.minecraft.network.protocol.game.ClientboundPlayerInfoUpdatePacket.Action
+import net.minecraft.network.protocol.game.ClientboundPlayerInfoUpdatePacket.Entry
 import net.minecraft.network.protocol.game.ClientboundSetCameraPacket
 import net.minecraft.server.MinecraftServer
 import net.minecraft.server.level.ServerLevel
@@ -28,6 +29,7 @@ import net.minecraft.world.entity.EntityType
 import net.minecraft.world.level.GameType
 import net.minecraft.world.phys.Vec2
 import net.minecraft.world.phys.Vec3
+import java.util.*
 import java.util.function.Consumer
 
 public class PlayerCamera(
@@ -97,5 +99,10 @@ public class PlayerCamera(
     private fun sendGamemodePacket(player: ServerPlayer, gamemode: GameType) {
         val packet = ClientboundGameEventPacket(ClientboundGameEventPacket.CHANGE_GAME_MODE, gamemode.id.toFloat())
         player.connection.send(packet)
+        val action = EnumSet.of(Action.UPDATE_GAME_MODE)
+        val entry = listOf(
+            Entry(player.uuid, null, false, 0, gamemode, null, false, 0, null)
+        )
+        player.connection.send(ClientboundPlayerInfoUpdatePacket(action, entry))
     }
 }
